4739a89113c0d617b63152dce5b2940b51360134
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-10 17:56:39 -0300

    defines class Email. Implements .isValid() to check if all fields are provided

--- src/main/java/model/Email.java
+++ src/main/java/model/Email.java
@@ -1,5 +1,45 @@
 package model;
 
-public class Email {
+import java.io.Serializable;
+
+public class Email implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String firstName = "";
+	private String lastName = "";
+	private String emailAddress = "";
+	
+	public Email() {
+		super();
+	};
+	
+	public Email(String firstName, String lastName, String emailAddress) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailAddress = emailAddress;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public boolean isValid() {
+		if (this.firstName != null && this.lastName != null && this.emailAddress != null &&
+			!this.firstName.isEmpty() && !this.lastName.isEmpty() && !this.emailAddress.isEmpty()) {
+			
+			return true;
+			
+		}
+		return false;
+	}
 
 }
--- test/model/EmailTest.java
+++ test/model/EmailTest.java
@@ -1,6 +1,8 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +11,20 @@ class EmailTest {
 	@Test
 	void isValidReturnsFalseWhenFirstNameIsNotProvided() {
 		
+		Email email = new Email();
+		email.setLastName("Costa");
+		email.setEmailAddress("contato@alejandrocosta.dev.br");
 		
+		assertFalse(email.isValid());
+		
+	}
+	
+	@Test
+	void isValidReturnsTrueWhenAllFieldsAreProvided() {
+		
+		Email email = new Email("Alejandro", "Costa", "contato@alejandrocosta.dev.br");
+		
+		assertTrue(email.isValid());
 		
 	}
 
