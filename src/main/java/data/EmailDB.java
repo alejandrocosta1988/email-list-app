fb3259fd65d7d526c7c88f06c8e51e3b256cd6f5
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-12 21:21:33 -0300

    inserts new email in the database

---
+++ src/main/java/data/EmailDB.java
@@ -0,0 +1,44 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.Email;
+
+public class EmailDB {
+
+	public static int insertEmail(Email email) {
+		
+		ConnectionPool pool = ConnectionPool.getInstance();
+		Connection connection = pool.getConnection();
+		PreparedStatement preparedStatement = null;
+		
+		String query = "INSERT INTO table_users (first_name, last_name, email) VALUES (?, ?, ?)";
+		
+		try {
+			
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, email.getFirstName());
+			preparedStatement.setString(2, email.getLastName());
+			preparedStatement.setString(3, email.getEmailAddress());
+			
+			return preparedStatement.executeUpdate();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+			return 0;
+			
+		} finally {
+			
+			try {
+				preparedStatement.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			pool.freeConnection(connection);
+			
+		}
+	}
+}
