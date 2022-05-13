48da4d4642a01a0d2e22dce24aa84bea1cbbf2c3
Alejandro Costa <alejandroesfc@gmail.com>
2022-05-13 06:05:05 -0300

    refactors definition of message and email user. Adds new else if to see registered

--- src/main/java/servlets/EmailListServlet.java
+++ src/main/java/servlets/EmailListServlet.java
@@ -24,6 +24,7 @@ public class EmailListServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String url = "/index.jsp";
+		String message = "";
 		
 		String action = request.getParameter("action");
 		if (action == null || action.isEmpty()) {
@@ -36,12 +37,12 @@ public class EmailListServlet extends HttpServlet {
 			
 		} else if (action.equals("register")) {
 		
-			String message = "";
 			String firstName = request.getParameter("first-name");
 			String lastName = request.getParameter("last-name");
 			String emailAddress = request.getParameter("email");
 			
 			Email email = new Email(firstName, lastName, emailAddress);
+			request.setAttribute("email", email);
 			
 			if (EmailDB.emailExistsInDataBase(email)) {
 				
@@ -55,11 +56,12 @@ public class EmailListServlet extends HttpServlet {
 				
 			}
 			
-			request.setAttribute("message", message);
-			request.setAttribute("email", email);
-			getServletContext().getRequestDispatcher(url).forward(request, response);
+		} else if (action.equals("see-registered")) {
 			
 		}
+		
+		request.setAttribute("message", message);
+		getServletContext().getRequestDispatcher(url).forward(request, response);
 		
 	}
 
